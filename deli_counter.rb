6caab4dbafa8f3d6b8a84899a66e045095b495f3
Jordan Harris <jharris@wework.com>
2018-06-26 15:45:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,16 +3,16 @@ require "pry"
 
 # katz_deli = []
 
-def take_a_number(arr, name)
-  num_in_line = arr.length
-  if name != "" 
-  num_in_line = num_in_line + 1
-  arr = arr.push.("#{num_in_line}. #{name}")
-  num_in_line = arr.length % 2
-  puts "Welcome, #{name}. You are number #{num_in_line} in line."
-  return arr
-  end
-end
+# def take_a_number(arr, name)
+#   num_in_line = arr.length
+#   if name != "" 
+#   num_in_line = num_in_line + 1
+#   arr = arr.push.("#{num_in_line}. #{name}")
+#   num_in_line = arr.length % 2
+#   puts "Welcome, #{name}. You are number #{num_in_line} in line."
+#   return arr
+#   end
+# end
 
 def now_serving(katz_deli)
   puts "Currently serving #{kat}."
