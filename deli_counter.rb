c81800fd784d6a60cb6015c08dbd73f28031695d
Jordan Harris <jharris@wework.com>
2018-06-26 15:31:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ require "pry"
 
 def take_a_number(arr, name)
   num_in_line = arr.length
-  if num_in_line != 0
+  if name != ""
   num_in_line = num_in_line + 1
   arr = arr.push.("#{num_in_line}. #{name}")
   puts "Welcome, #{name}. You are number #{num_in_line} in line."
