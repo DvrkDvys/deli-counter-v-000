395b92be7b587bc2e368c3ad7f06532bc930e688
Jordan Harris <jharris@wework.com>
2018-06-26 15:53:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,6 +22,7 @@ def take_a_number(arr, name)
 end
 
 def now_serving(arr)
+  if arr.length == 0  
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli.shift}."
