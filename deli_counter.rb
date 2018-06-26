accb2e772cbf74195cb2726e9cd4b0c35632c80e
Jordan Harris <jharris@wework.com>
2018-06-26 16:01:30 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,16 +5,16 @@ def take_a_number(arr, name)
   arr << name
   info = "Welcome, #{name}. You are number #{arr.length} in line."
   puts info
-  return info
+  # return info
 end
 
 def now_serving(arr)
   if arr.length == 0  
     puts "There is nobody waiting to be served!"
   else
-    info = "Currently serving #{katz_deli.shift}."
+    info = "Currently serving #{arr.shift}."
     puts info
-    return info
+    # return info
   end
 end
  
