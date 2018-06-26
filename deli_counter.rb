9fb536927262b006ab0c41e33f56b119b2f04909
Jordan Harris <jharris@wework.com>
2018-06-26 15:49:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,7 @@ require "pry"
 
 def take_a_number(arr, name)
   arr << name
+  puts "Welcome, #{name}. You are number #{arr.length} in line."
   return "Welcome, #{name}. You are number #{arr.length} in line."
 end
 
