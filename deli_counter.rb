5b2c58f73f1a353665aa0647ec083600f5ac0c20
Jordan Harris <jharris@wework.com>
2018-06-26 14:40:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,9 @@ def take_a_number(katz_deli, "Matz")
 end
 
 def now_serving(katz_deli) #=> "Currently serving Ada."
-
+  puts "Currently serving #{}."
 end
  
-line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent"
+def line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent"
+
+end
