0deff9f4fa1b825cecbd4d480da5c4f714abeb02
Jordan Harris <jharris@wework.com>
2018-06-26 15:58:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,8 +19,11 @@ end
 def line(arr)
   if rest_arr.length == 0 
     puts "The line is currently empty."
-  else line = rest_arr.join(" ")
-    reply="The line is currently:"
-    puts "The line is currently: #{line}"
+  else 
+    katz_deli.each_with_index do |value, index|
+      message += " #{index.to_i+1}. #{value}"
+    end
+
+    puts "#{message}"
   end
 end
